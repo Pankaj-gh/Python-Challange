6ebdda82a6120de948659ace3966d56e6cbf6951
Pankaj <tahiliani.pankaj19@gmail.com>
2020-08-17 08:39:04 -0500

    calculated total votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,9 +1,19 @@
 import os
 import csv
 import statistics
+file_path = os.path.join('Resources','election_data.csv')
+with open(file_path,'r') as mainfile:
+    pypoll = csv.reader(mainfile, delimiter=',')
+    header = next(pypoll)
+    print(header)
 
-def pypoll(data_csv):
-    file_path = os.path.join('Resources','data_csv'):
-    with open(file_path,'r') as mainfile:
-        py_poll = csv.reader(mainfile, delimiter=',')
-        print(pypoll[0])
+    py_poll=[]
+    for line in pypoll:
+        py_poll.append(line)
+    
+
+print("*"*80)
+print("Calculating total number of votes")
+
+total_votes = len(py_poll)
+print("Total Votes: {}".format(total_votes))
