9126032609bc62e54aad6ef08ee37385d9efc6d7
Pankaj <tahiliani.pankaj19@gmail.com>
2020-08-16 22:40:24 -0500

    avg change pl

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+from statistics import mean
 file_path =os.path.join('Recources','budget_data.csv')
 
 with open (file_path,'r') as csvfile:
@@ -24,6 +25,21 @@ convertrow = [int(row[1]) for row in bankfile]
 netpl= sum(convertrow)
 print(netpl)
 
+#Avg of Change in Profit/Loss
+change = convertrow[-1]-convertrow[0]
+print(change)
+avgchange = (change / (len(convertrow)-1))
+print(avgchange)
 
 
-
+profits = [row for row in convertrow if row>0]
+losses=[row for row in convertrow if row<0]
+totalprofit = sum(profits)
+avgprofit = mean(profits)
+totalloss = sum(losses)
+avgloss = mean(losses)
+print(totalprofit)
+print(totalloss)
+print(avgloss)
+print(avgprofit)
+print(avgprofit+avgloss)
