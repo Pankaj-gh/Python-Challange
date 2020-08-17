13fbeebea22363d5c96ee15aa722d32dc6a62485
Pankaj <tahiliani.pankaj19@gmail.com>
2020-08-17 00:50:02 -0500

    completed 4 questions

--- PyBank/main.py
+++ PyBank/main.py
@@ -23,23 +23,56 @@ print(len(months))
 
 convertrow = [int(row[1]) for row in bankfile]
 netpl= sum(convertrow)
-print(netpl)
+#print(netpl)
 
 #Avg of Change in Profit/Loss
 change = convertrow[-1]-convertrow[0]
-print(change)
+#print(change)
 avgchange = (change / (len(convertrow)-1))
 print(avgchange)
 
 
-profits = [row for row in convertrow if row>0]
-losses=[row for row in convertrow if row<0]
-totalprofit = sum(profits)
-avgprofit = mean(profits)
-totalloss = sum(losses)
-avgloss = mean(losses)
-print(totalprofit)
-print(totalloss)
-print(avgloss)
-print(avgprofit)
-print(avgprofit+avgloss)
+#convert to int for bankfile
+bankfileconverted = [[row[0],int(row[1])] for row in bankfile]
+print(bankfileconverted)
+maxprofit= max(bankfileconverted, key=lambda y:y[1])
+print(maxprofit)
+print(y)
+ind=0
+#finding index value of maxprofit
+for i in range(len(convertrow)):
+    if convertrow[i]==y:
+        print(ind)
+        break
+    else:
+        ind+=1
+
+#print(bankfileconverted[ind])
+
+#calculating greatest increase in profit from ind and ind-1
+
+greatest_profit_increase = convertrow[ind]-convertrow[ind-1]
+print("Greatest Increase in Profits: " + bankfileconverted[ind][0] + " (${})".format(greatest_profit_increase))
+
+#Calclulate greatest loss
+
+maxloss= min(bankfileconverted,key=lambda z:z[1])
+print(maxloss)
+
+print(z)
+lossind = 0
+
+for i in range(len(convertrow)):
+    if convertrow[i]==z:
+        print(lossind)
+        break
+    else:
+        lossind+=1
+
+print(bankfileconverted[lossind])
+
+greatest_profit_decrease = convertrow[lossind]-convertrow[lossind-1]
+print("Greatest Decrease in Profits: " + bankfileconverted[lossind][0] + " (${})".format(greatest_profit_decrease))
+
+
+
