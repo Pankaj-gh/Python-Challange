038eed4c1d3936ec34a2eeb231ee94ba4477d066
Pankaj <tahiliani.pankaj19@gmail.com>
2020-08-19 17:05:26 -0500

    improving aesthetics

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,44 +6,48 @@ file_path =os.path.join('Recources','budget_data.csv')
 with open (file_path,'r') as csvfile:
     pybank = csv.reader(csvfile,delimiter=',')
     header = next(pybank)
-    print(header)
+    #print(header)
     bankfile = []
 
     for row in pybank:
         bankfile.append(row)
-print(bankfile)
+#print(bankfile[0])
 
 
 #Total Months
 
-months = [row[0] for row in bankfile]
-print(len(months))
+print("Financial Analysis")
+print("-"*50)
 
-#Net Profit/Loss
+months = [row[0] for row in bankfile]
+#print(len(months))
+ans1=("Total Months: {}".format(len(months)))
+print(ans1)
 
 convertrow = [int(row[1]) for row in bankfile]
 netpl= sum(convertrow)
-#print(netpl)
+ans2=("Total: ${}".format(netpl))
+print(ans2)
 
 #Avg of Change in Profit/Loss
 change = convertrow[-1]-convertrow[0]
 #print(change)
 avgchange = (change / (len(convertrow)-1))
-print(avgchange)
-
+ans3=('Average Change: ${}'.format(round(avgchange,2)))
+print(ans3)
 
 #convert to int for bankfile
 bankfileconverted = [[row[0],int(row[1])] for row in bankfile]
 #print(bankfileconverted)
 maxprofit= max(bankfileconverted, key=lambda y:y[1])
-print(maxprofit)
+#print(maxprofit)
 y = maxprofit[1]
-print(y)
+#print(y)
 ind=0
 #finding index value of maxprofit
 for i in range(len(convertrow)):
     if convertrow[i]==y:
-        print(ind)
+        #print(ind)
         break
     else:
         ind+=1
@@ -53,19 +57,19 @@ for i in range(len(convertrow)):
 #calculating greatest increase in profit from ind and ind-1
 
 greatest_profit_increase = convertrow[ind]-convertrow[ind-1]
-print("Greatest Increase in Profits: " + bankfileconverted[ind][0] + " (${})".format(greatest_profit_increase))
-
+ans4=("Greatest Increase in Profits: " + bankfileconverted[ind][0] + " (${})".format(greatest_profit_increase))
+print(ans4)
 #Calclulate greatest loss
 
 maxloss= min(bankfileconverted,key=lambda z:z[1])
-print(maxloss)
+#print(maxloss)
 z=maxloss[1]
-print(z)
+#print(z)
 lossind = 0
 
 for i in range(len(convertrow)):
     if convertrow[i]==z:
-        print(lossind)
+        #print(lossind)
         break
     else:
         lossind+=1
@@ -73,7 +77,7 @@ for i in range(len(convertrow)):
 #print(bankfileconverted[lossind])
 
 greatest_profit_decrease = convertrow[lossind]-convertrow[lossind-1]
-print("Greatest Decrease in Profits: " + bankfileconverted[lossind][0] + " (${})".format(greatest_profit_decrease))
-
+ans5=("Greatest Decrease in Profits: " + bankfileconverted[lossind][0] + " (${})".format(greatest_profit_decrease))
+print(ans5)
 
 
